afd854a7ce1f306bdf621ece4c7acd163c44bcec
GVisy <vglibin@mail.ru>
2022-01-04 22:28:55 +0700

    ty
    HM_2

---
+++ Glibin_Vladimir_HW_2/task_2_1.py
@@ -0,0 +1,12 @@
+# Задание 1
+# Выяснить тип результата выражений:
+
+# print(type(15 * 3))
+# print(type(15 / 3))
+# print(type(15 // 2))
+# print(type(15 ** 2))
+a = (15 * 3)
+b = (15 / 3)
+c = (15 // 2)
+d = (15 ** 2)
+print(type(a), a, "\n", type(b), b, "\n", type(c), c, "\n", type(d), d)
