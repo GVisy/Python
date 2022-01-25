5226a04d40995468e1d8c66c53ddd32711f9db79
GVisy <vglibin@mail.ru>
2022-01-25 22:53:53 +0700

    HW_6

---
+++ Glibin_Vladimir_HW_6/task_6_1.py
@@ -0,0 +1,19 @@
+from pprint import pprint
+
+
+def get_parse_attrs(line: str) -> tuple:
+    """Парсит строку на атрибуты и возвращает кортеж атрибутов (<remote_addr>, <request_type>, <requested_resource>)"""
+    remote_addr = line.split('- -')[0]
+    request_type = line.split('] "')[1].split(' /')[0]
+    requested_resource = line.split(' /')[1].split(' HT')[0]
+    return remote_addr, request_type, requested_resource
+
+
+list_out = list()
+with open('nginx_logs.txt', 'r', encoding='utf-8') as fr:
+    while True:
+        line = fr.readline()
+        if not line:
+            break
+        list_out.append(get_parse_attrs(line))
+pprint(list_out)
